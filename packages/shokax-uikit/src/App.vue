530deb54d2cbaa80ec44f46a8b65ebccf1713b71
zkz098 <zkz20081204@126.com>
2025-04-18 19:55:21 +0800

    refactor: monorepo化 & 添加turborepo

---
+++ packages/shokax-uikit/src/App.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import type { NavItemType } from './components/navbar/NavTypes'
+import { provide } from 'vue'
+import CodeBlock from './components/CodeBlock.vue'
+import DividerLine from './components/DividerLine.vue'
+import NavBar from './components/navbar/NavBar.vue'
+import ToolBar from './components/toolbar/ToolBar.vue'
+import Waves from './components/waves/Waves.vue'
+
+const navlinks: NavItemType[] = [
+  {
+    href: '/',
+    text: 'Home',
+    icon: 'i-ri-home-line',
+  },
+  {
+    href: '/about',
+    text: 'About',
+    icon: 'i-ri-user-6-line',
+  },
+  {
+    href: '/contact',
+    text: 'Contact',
+    icon: 'i-ri-mail-line',
+  },
+  {
+    dropbox: true,
+    text: 'Account',
+    icon: 'i-ri-account-circle-line',
+    dropboxItems: [
+      {
+        href: '/login',
+        text: 'Login',
+        icon: 'i-ri-login-circle-line',
+      },
+      {
+        href: '/register',
+        text: 'Register',
+        icon: 'i-ri-survey-line',
+      },
+    ],
+  },
+]
+
+provide('linkElName', 'a')
+</script>
+
+<template>
+  <!-- <Brand :is-home="true" art-board-content="111" headline-content="222" /> -->
+  <NavBar :nav-links="navlinks" name="website" :click-toggle-callback="() => void 0" />
+  <ToolBar :show-contents-click-callback="() => void 0" />
+  <Waves />
+
+  <DividerLine content="111" />
+  <CodeBlock>
+    <pre class="shiki shiki-themes vitesse-light vitesse-dark" style="background-color:#ffffff;--shiki-dark-bg:#121212;color:#393a34;--shiki-dark:#dbd7caee" tabindex="0"><code class="language-shell">
+  <span class="line"><span style="color:#59873A;--shiki-dark:#80A665">apt</span><span style="color:#B56959;--shiki-dark:#C98A7D"> install</span><span style="color:#B56959;--shiki-dark:#C98A7D"> build-essential</span><span style="color:#B56959;--shiki-dark:#C98A7D"> ca-certificates</span><span style="color:#B56959;--shiki-dark:#C98A7D"> zlib1g-dev</span><span style="color:#B56959;--shiki-dark:#C98A7D"> libpcre3</span><span style="color:#B56959;--shiki-dark:#C98A7D"> libpcre3-dev</span><span style="color:#B56959;--shiki-dark:#C98A7D"> tar</span><span style="color:#B56959;--shiki-dark:#C98A7D"> unzip</span><span style="color:#B56959;--shiki-dark:#C98A7D"> libssl-dev</span><span style="color:#B56959;--shiki-dark:#C98A7D"> wget</span><span style="color:#B56959;--shiki-dark:#C98A7D"> curl</span><span style="color:#B56959;--shiki-dark:#C98A7D"> git</span><span style="color:#B56959;--shiki-dark:#C98A7D"> cmake</span><span style="color:#B56959;--shiki-dark:#C98A7D"> ninja-build</span><span style="color:#B56959;--shiki-dark:#C98A7D"> golang</span></span>
+  <span class="line"><span style="color:#A0ADA0;--shiki-dark:#758575DD"># 需要启用源代码软件源(deb-src)</span></span>
+  <span class="line"><span style="color:#59873A;--shiki-dark:#80A665">apt-get</span><span style="color:#B56959;--shiki-dark:#C98A7D"> build-dep</span><span style="color:#B56959;--shiki-dark:#C98A7D"> nginx</span></span></code></pre>
+  </CodeBlock>
+  <div id="comment">
+    111
+  </div>
+  <div id="toc" />
+</template>
+
+<style>
+@import url('./styles/style.css');
+@import url('./styles/palette.css');
+</style>
---
+++ packages/shokax-uikit/src/components/CodeBlock.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="box-shadow rounded-lg text-4">
+    <slot />
+  </div>
+</template>
+
+<style lang="css" scoped>
+@font-face {
+  font-family: 'JetBrains Mono';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src:
+    url('../assets/fonts/JetBrainsMono-Regular.woff2') format('woff2'),
+    url('../assets/fonts/JetBrainsMono-Regular.ttf') format('truetype');
+}
+
+.box-shadow {
+  box-shadow: 0.5rem 0.5rem 1rem var(--grey-3);
+  background-color: var(--grey-5);
+}
+
+:deep(*) {
+  font-family: 'JetBrains Mono', 'Courier New', Courier, monospace;
+  font-size: 1rem;
+  line-height: 1.5rem;
+}
+
+:deep(code) {
+  counter-reset: step;
+  counter-increment: step 0;
+}
+
+:deep(code .line::before) {
+  content: counter(step);
+  counter-increment: step;
+  width: 1rem;
+  margin-right: 1.5rem;
+  display: inline-block;
+  text-align: right;
+  color: rgba(115, 138, 148, 0.4);
+}
+
+:deep(.diff .remove) {
+  background-color: var(--color-red);
+  opacity: 0.7;
+}
+
+:deep(.diff .remove)::before {
+  content: '-';
+  color: var(--color-red);
+  font-weight: bold;
+}
+
+:deep(.diff .add) {
+  background-color: var(--color-green);
+}
+
+:deep(.diff .add)::before {
+  content: '+';
+  color: var(--color-green);
+  font-weight: bold;
+}
+
+:deep(.diff .highlighted) {
+  background-color: var(--grey-4);
+}
+</style>
