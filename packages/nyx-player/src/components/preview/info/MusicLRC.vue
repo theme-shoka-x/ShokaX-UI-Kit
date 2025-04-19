c85c8517e67b729fd043ce5dbaa6e36a6c940890
zkz098 <zkz20081204@126.com>
2025-04-19 23:31:37 +0800

    refactor: 优化歌词显示效果

--- packages/nyx-player/src/components/preview/info/MusicLRC.vue
+++ packages/nyx-player/src/components/preview/info/MusicLRC.vue
@@ -8,7 +8,6 @@ const lrcIdx = ref(0)
 const lrcRes = ref([] as LyricLine[])
 
 const playingStore = usePlayingStore()
-const currentTime = computed(() => playingStore.currentTime)
 
 const lrcUrl = computed(() => {
   const currentPlaylist = playingStore.getCurrentPlaylist()
@@ -26,21 +25,66 @@ watch(lrcUrl, async (url) => {
     lrcRes.value = lrc.lyrics
   }
 }, { immediate: true })
+
+const showLyric = ref<LyricLine[]>([])
+let idx = 0
+
+watch(() => playingStore.currentTime, (time) => {
+  // 寻找当前播放所在的歌词索引
+  const currentIdx = lrcRes.value.findIndex(line => time >= line.start && time <= line.end)
+
+  // 若找到并且与上一次不同，更新 idx 和窗口
+  if (currentIdx !== -1 && currentIdx !== idx) {
+    idx = currentIdx
+
+    // 从当前行开始取接下来的 4 行歌词（包括当前行）
+    showLyric.value = lrcRes.value.slice(idx, idx + 4)
+  }
+})
+
+watch(() => playingStore.getCurrentPlaylist().getCurrentSong(), () => {
+  showLyric.value = []
+  lrcIdx.value = 0
+})
 </script>
 
 <template>
   <div class="lrc">
-    <div class="inner" :style="{ transform: `translateY(-${lrcIdx}rem)` }">
-      <template v-for="(lyric, index) in lrcRes" :key="index">
-        <p :class="{ current: currentTime >= lyric.start && currentTime <= lyric.end }" :data-index="index" @transitionstart="lrcIdx = index">
-          {{ lyric.text }}
-        </p>
-      </template>
+    <div class="inner">
+      <TransitionGroup name="list" tag="ul" class="p-0">
+        <li class="list-none">
+          <p
+            v-for="(lyric, index) in showLyric"
+            :key="lyric.start"
+            :class="{ current: index === lrcIdx }"
+          >
+            {{ lyric.text }}
+          </p>
+        </li>
+      </TransitionGroup>
     </div>
   </div>
 </template>
 
 <style scoped>
+.list-move, /* 对移动中的元素应用的过渡 */
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
+/* 确保将离开的元素从布局流中删除
+  以便能够正确地计算移动的动画。 */
+.list-leave-active {
+  position: absolute;
+}
+
 .lrc {
   max-height: 4rem;
   margin-top: 0.3125rem;
